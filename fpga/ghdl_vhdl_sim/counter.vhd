700e67a17d5b7eff4c603cc42ed9a11b2e600b43
IoT Engineering Education @ KMUTNB <iot-kmutnb@users.noreply.github.com>
2023-07-09 13:42:13 +0700

    update fpga

---
+++ fpga/ghdl_vhdl_sim/counter.vhd
@@ -0,0 +1,31 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity counter is
+  generic( WIDTH : natural := 8 );
+  port(
+    CLK  : in std_logic;
+    nRST : in std_logic;
+    CE   : in std_logic;
+    Q    : out std_logic_vector( WIDTH-1 downto 0 )
+  );
+end counter;
+
+architecture behave of counter is
+  constant ZEROS : unsigned(WIDTH-1 downto 0) := (others => '0');
+  signal    regs : unsigned(WIDTH-1 downto 0) := ZEROS;
+begin
+  process (nRST, CLK) 
+  begin
+    if nRST = '0' then
+      regs <= ZEROS;    -- reset the register to zero
+    elsif rising_edge(CLK) and CE = '1' then
+      regs <= regs + 1; -- increment the register by 1
+    end if;
+  end process;
+  
+  Q <= std_logic_vector(regs);
+
+end behave;
+
---
+++ fpga/ghdl_vhdl_sim/counter_tb.vhd
@@ -0,0 +1,53 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity counter_tb is
+  -- empty
+end counter_tb;
+
+architecture testbench of counter_tb is
+
+  -- This is the VHDL component that will be instantiated.
+  component counter is
+    generic( WIDTH : natural := 8 );
+    port(
+      CLK  : in std_logic;
+      nRST : in std_logic;
+      CE   : in std_logic;
+      Q    : out std_logic_vector( WIDTH-1 downto 0 ) 
+    );
+  end component;
+  
+  -- internal test signals
+  constant WIDTH : natural := 8;
+  signal t_nRST, t_CLK, t_CE : std_ulogic := '0';
+  signal t_Q : std_logic_vector( WIDTH-1 downto 0 );
+
+begin
+  -- Instantiate the upcounter as DUT (design under test).
+  DUT: counter 
+    generic map ( WIDTH => 8 )
+    port map( CLK => t_CLK, nRST => t_nRST, CE => t_CE, Q => t_Q );
+
+  -- create a reset signal
+  process begin 
+     t_nRST <= '0';
+     wait for 100 ns;
+     t_nRST <= '1';
+     wait; -- wait forever
+  end process;
+
+  -- create a 50MHz clock (periodic) signal
+  process begin 
+     t_CLK <= '0';
+     wait for 10 ns;
+     t_CLK <= '1';
+     wait for 10 ns;
+  end process;
+   
+  -- create a clock enable (CE) signal
+  t_CE <= '0', '1' after 200 ns, '0' after 10 us;
+ 
+end testbench;
+
